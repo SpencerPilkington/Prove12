bdbe64a85c4348a2266aa4503d3b740a81a5d2d9
Spencer <spencer@foo.com>
2021-07-10 22:00:13 -0600

    prove12 final

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const session = require('express-session')
 
-const PORT = process.env.PORT // So we can run on heroku || (OR) localhost:5000
+const PORT = process.env.PORT || 5000// So we can run on heroku || (OR) localhost:5000
 
 const app = express()
 
